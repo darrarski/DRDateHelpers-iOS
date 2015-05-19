463d9fe6c957d4d5b573b2e6fe2197aef86a2d0a
Dariusz Rybicki <darrarski@gmail.com>
2015-05-19 20:51:40 +0200

    Initial commit

---
+++ DRDateHelpersExample/Supporting Files/main.m
@@ -0,0 +1,13 @@
+//
+//  Created by Dariusz Rybicki on 19/05/15.
+//  Copyright (c) 2015 Darrarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ DRDateHelpersExampleTests/DRDateHelpersExampleTests.m
@@ -0,0 +1,19 @@
+//
+//  Created by Dariusz Rybicki on 19/05/15.
+//  Copyright (c) 2015 Darrarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface DRDateHelpersExampleTests : XCTestCase
+
+@end
+
+@implementation DRDateHelpersExampleTests
+
+- (void)testExample {
+    XCTAssert(YES, @"Pass");
+}
+
+@end
